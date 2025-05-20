e523eef5618ca0bf48f7d99355298c6c6434db5d
Mishawaka <robertzanat@yandex.ru>
2025-05-20 17:13:28 +0300

    init

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ const input = document.querySelector(".search__input");
 const button = document.querySelector(".search__button");
 const select = document.querySelector(".search__select");
 
-addEventListener('click', redirection);
+button.addEventListener('click', redirection);
 
 function redirection(event) {
     event.preventDefault();
 
     const value = input.value;
     const selectedBrowse = select.value;
+
+    let params;
+
+    if (selectedBrowse == "google" || selectedBrowse == "bing") {
+        params = "search?q=";
+    } else if (selectedBrowse == "yandex") {
+        params = "search?text=";
+    } else if (selectedBrowse == "duckduckgo") {
+        params = "?q=";
+    }
+
+    window.location.href = `https://www.${selectedBrowse}.com/${params}${value}`;
 }
